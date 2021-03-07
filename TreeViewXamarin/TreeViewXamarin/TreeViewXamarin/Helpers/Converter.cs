093aa7a8da80eda8f34cd305a60eb1f1ba9dc931
lakshmi.natarajan <lakshmi.natarajan@syncfusion.com>
2021-03-08 00:01:56 +0530

    XAMARIN-38721 Sample added

---
+++ TreeViewXamarin/TreeViewXamarin/TreeViewXamarin/Helpers/Converter.cs
@@ -0,0 +1,23 @@
+﻿using Syncfusion.TreeView.Engine;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Styling
+{
+    public class LabelIsVisibleConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var mailsCount = (int)value;
+            return mailsCount > 0 ? true : false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TreeViewXamarin/TreeViewXamarin/TreeViewXamarin/Model/MailFolder.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Text;
+
+namespace Styling
+{
+    public class MailFolder : INotifyPropertyChanged
+    {
+        #region Feilds
+
+        private string folderName;
+        private int mailsCount;
+        private ObservableCollection<MailFolder> subFolders;
+
+        #endregion
+
+        #region Constructor
+        public MailFolder()
+        {
+        }
+
+        #endregion
+
+        #region Properties
+        public ObservableCollection<MailFolder> SubFolder
+        {
+            get { return subFolders; }
+            set
+            {
+                subFolders = value;
+                RaisedOnPropertyChanged("SubFolder");
+            }
+        }
+
+        public string FolderName
+        {
+            get { return folderName; }
+            set
+            {
+                folderName = value;
+                RaisedOnPropertyChanged("FolderName");
+            }
+        }
+        public int MailsCount
+        {
+            get { return mailsCount; }
+            set
+            {
+                mailsCount = value;
+                RaisedOnPropertyChanged("MailsCount");
+            }
+        }
+
+        #endregion
+
+        #region INotifyPropertyChanged
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void RaisedOnPropertyChanged(string _PropertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(_PropertyName));
+            }
+        }
+
+        #endregion
+    }
+}
